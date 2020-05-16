8ff20ffacff335794a0f496cc97da577b76921f9
vicko pratama ibrahim <37886885+vickokakuse21@users.noreply.github.com>
2020-05-16 11:50:42 +0700

    connect to mongoose
    create user admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import data from './data';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 function App() {
   const openMenu = () => {
