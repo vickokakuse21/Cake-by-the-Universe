5ce911c9864467ba16bc7ba4b440769af19a208f
muhammad <37789994+fiayvv@users.noreply.github.com>
2020-05-17 05:18:57 +0700

    Sign-in

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/cake-by-the-universe'
+    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/cake-by-the-universe',
+    JWT_SECRET : process.env.JWT_SECRET || 'somethingsecret'
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import data from './data';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import CartScreen from './screens/CartScreen';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import CartScreen from './Screens/CartScreen';
+import SigninScreen from './Screens/SigninScreen';
 
 function App() {
   const openMenu = () => {
@@ -25,7 +26,8 @@ function App() {
         </div>
         <div className="header-links">
           <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <Link to="/signin">Sign In</Link>
+          {/* <a href="signin.html">Sign In</a> */}
         </div>
       </header>
       <aside className="sidebar">
@@ -44,6 +46,7 @@ function App() {
       </aside>
       <main className="main">
         <div className="content">
+          <Route path="/signin" component={SigninScreen} />
           <Route path="/product/:id" component={ProductScreen}/>
           <Route path="/cart/:id?" component={CartScreen}/>
           <Route path="/" exact = {true} component={HomeScreen}/>
